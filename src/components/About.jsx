27861ad14c8fa486982fd254805552435cd7b292
unknown <nicomugas@gmail.com>
2022-10-21 18:28:20 -0300

    actualizacion responsive varios

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,21 +12,25 @@ export default function About() {
             <div className={s.contenido}>
                 <div className={s.title}>SOBRE MI</div>
                 <div className={s.mail}>nicomugas@gmail.com</div>
-                
+
                 <div className={s.saludo}>Hola soy Nico Mugas</div>
                 <div className={s.title2}>ANALISTA DE SISTEMAS || FULL STACK WEB DEVELOPER</div>
-                <div className={s.title2}>Me gusta mucho la tecnología, y estar en continuo aprendizaje. 
-                Recientemente he finalizado una capacitación en Node JS - <br /> JavaScript - React Redux, y actualmente me interesaría seguir<br /> desarrollandome en dichas tecnologias, sobre la base de mi<br /> carrera informática adquirida.
-                Altamente comprometido con los proyectos y el trabajo en equipo. </div>
+                <div className={s.title2}>
+                    
+                    Te cuento que me gusta mucho la tecnología y la programación, y esto me obliga a estar en continuo aprendizaje.
+                    Recientemente he finalizado una capacitación en Node JS - JavaScript - React - Redux, y  me interesaría seguir
+                     desarrollandome en dichas tecnologias, sobre la base de mi carrera informática adquirida.
+                    Altamente comprometido con los proyectos, y el trabajo en equipo.
+                </div>
 
                 <div><h3>Skills</h3></div>
-               
+
                 <div>
                     <SkillsBar />
                 </div>
             </div>
-             <div className={s.hijo2}> <img src={foto} alt="nn" width="400px" /></div> 
-       
+            <div className={s.hijo2}> <img src={foto} alt="nn" width="400px" /></div>
+
 
 
         </div>
